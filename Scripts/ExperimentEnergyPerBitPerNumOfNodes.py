a4da1b19d3d53177fde90591154ac0e2d8e2106a
Gilles Callebaut <callebaut.gilles@gmail.com>
2018-02-20 17:58:25 +0100

    updated experiment

--- Scripts/ExperimentEnergyPerBitPerNumOfNodes.py
+++ Scripts/ExperimentEnergyPerBitPerNumOfNodes.py
@@ -25,8 +25,10 @@ mean_energy_per_bit_list = []
 mean_unique_packets_sent_list = []
 mean_packets_sent_list = []
 num_nodes_list = []
+total_packets_sent = []
+packets_received_by_gateway = []
 
-for num_nodes in range(1, 1000, 100):
+for num_nodes in range(10, 1000, 100):
     print('{} nodes in network'.format(num_nodes))
 
     tx_power_mW = {2: 91.8, 5: 95.9, 8: 101.6, 11: 120.8, 14: 146.5}  # measured TX power for each possible TP
@@ -40,17 +42,14 @@ for num_nodes in range(1, 1000, 100):
 
     for node_id in range(num_nodes):
         location = Location(min=0, max=Config.CELL_SIZE, indoor=False)
-        # location = Location(x=60, y=60, indoor=True)
-        # TODO check if random location is more than 1m from gateway
-        # node = Node(id, EnergyProfile())
         energy_profile = EnergyProfile(5.7e-3, 15, tx_power_mW,
                                        rx_power={'pre_mW': 8.2, 'pre_ms': 3.4, 'rx_lna_on_mW': 39, 'rx_lna_off_mW': 34,
                                                  'post_mW': 8.3, 'post_ms': 10.7})
         lora_param = LoRaParameters(freq=np.random.choice(LoRaParameters.DEFAULT_CHANNELS),
                                     sf=np.random.choice(LoRaParameters.SPREADING_FACTORS),
                                     bw=125, cr=5, crc_enabled=1, de_enabled=0, header_implicit_mode=0, tp=14)
-        node = Node(node_id, energy_profile, lora_param, 1000 * 60*60, process_time=5, adr=True, location=location,
-                    base_station=gateway, env=env, payload_size=12, air_interface=air_interface)
+        node = Node(node_id, energy_profile, lora_param, 1000000*2, process_time=5, adr=True, location=location,
+                    base_station=gateway, env=env, payload_size=20*2, air_interface=air_interface)
         nodes.append(node)
         env.process(node.run())
         # plt.scatter(location.x, location.y, color='blue')
@@ -79,6 +78,8 @@ for num_nodes in range(1, 1000, 100):
         # print('E/bit {}'.format(energy_per_bit))
 
     num_nodes_list.append(num_nodes)
+    total_packets_sent.append(mean_packets_sent) # assign befor / num_nodes
+    packets_received_by_gateway.append(gateway.num_of_packet_received)
 
     mean_energy_per_bit = mean_energy_per_bit / num_nodes
     mean_unique_packets_sent = mean_unique_packets_sent / num_nodes
@@ -88,20 +89,27 @@ for num_nodes in range(1, 1000, 100):
     mean_unique_packets_sent_list.append(mean_unique_packets_sent)
     mean_packets_sent_list.append(mean_packets_sent)
 
+
+
     print('E/bit {}'.format(mean_energy_per_bit))
     print('Unique packets {}'.format(mean_unique_packets_sent))
     print('Total packets {}'.format(mean_packets_sent))
 
-ax = plt.subplot(3, 1, 1)
+ax = plt.subplot(4, 1, 1)
 plt.plot(num_nodes_list, mean_energy_per_bit_list)
 ax.set_title('Mean energy per bit')
 
-ax = plt.subplot(3, 1, 2)
+ax = plt.subplot(4, 1, 2)
 plt.plot(num_nodes_list, mean_unique_packets_sent_list)
 ax.set_title('Mean unique packets sent')
 
-ax = plt.subplot(3, 1, 3)
-plt.plot(num_nodes_list, np.subtract(mean_packets_sent_list, mean_unique_packets_sent_list))
-ax.set_title('Mean extra packets sent')
+ax = plt.subplot(4, 1, 3)
+plt.plot(num_nodes_list, np.divide(np.subtract(mean_packets_sent_list, mean_unique_packets_sent_list),mean_unique_packets_sent_list))
+ax.set_title('Mean extra packets ratio sent')
+
+
+ax = plt.subplot(4, 1, 4)
+plt.plot(num_nodes_list, np.divide(packets_received_by_gateway,total_packets_sent)) # total number of packets sent (ioncluding retransimissions)
+ax.set_title('DER')
 
 plt.show()
