555173046cb6a9fd0707beea2fec285e76c6039a
Gilles Callebaut <callebaut.gilles@gmail.com>
2018-03-22 17:05:43 +0100

    extend path os

--- Scripts/Simulations/generate_locations.py
+++ Scripts/Simulations/generate_locations.py
@@ -1,4 +1,9 @@
 import pickle
+import sys
+import os
+
+sys.path.append(os.getcwd() + '../../LoRaEnergySim')
+
 from Location import Location
 
 num_locations = 100
@@ -12,12 +17,9 @@ for num_sim in range(num_of_simulations):
         locations.append(Location(min=0, max=cell_size, indoor=False))
     locations_per_simulation.append(locations)
 
-
 with open('locations.pkl', 'wb') as f:
     pickle.dump(locations_per_simulation, f)
-
 
 # just to test the code
 with open('locations.pkl', 'rb') as filehandler:
     print(pickle.load(filehandler))
-
