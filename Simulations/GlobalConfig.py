bf138ca773823b9e9455111f282b0288a0fc0e14
Gilles Callebaut <callebaut.gilles@gmail.com>
2018-08-27 10:49:57 +0200

    update

--- Simulations/GlobalConfig.py
+++ Simulations/GlobalConfig.py
@@ -4,18 +4,19 @@ import numpy as np
 start_with_fixed_sf = False
 start_sf = 7
 
-transmission_rate_id = '0.1'
-transmission_rate_bit_per_ms = (12*8)/(60*60*1000)  # 12*8 bits per hour (1 typical packet per hour)
-simulation_time = 24 * 60 * 60 * 1000 * 100
+scaling_factor = 2
+transmission_rate_id = str(scaling_factor)
+transmission_rate_bit_per_ms = scaling_factor*(12*8)/(60*60*1000)  # 12*8 bits per hour (1 typical packet per hour)
+simulation_time = 24 * 60 * 60 * 1000 * 100/scaling_factor
 cell_size = 1000
 adr = True
-confirmed_messages = True
+confirmed_messages = False
 
 payload_sizes = range(5, 55, 5)
 path_loss_variances = [7.9]  # [0, 5, 7.8, 15, 20]
 
 MAC_IMPROVEMENT = False
-num_locations = 500
+num_locations = 50
 num_of_simulations = 1000
 locations_file = "Locations/"+"{}_locations_{}_sim.pkl".format(num_locations, num_of_simulations)
 
